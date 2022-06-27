93e24eb9259b4caa88c763f91e4c7fea2377ec6a
luisg210 <gerardo.henriquez21@itca.edu.sv>
2022-06-27 16:03:16 -0600

    Second

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,42 @@
-# version change
-$ git init
-$ git add .
-$ git commit -m "My site ready for deployment."
+#!/usr/bin/env sh
 
-# creates a new app with a specified name
-$ heroku apps:create example
+# abort on errors
+set -e
 
-# set buildpack for static sites
-$ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-static.git
+# build
+npm run build
 
-# publish site
-$ git push heroku main
+# navigate into the build output directory
+cd dist
 
-# opens a browser to view the Dashboard version of Heroku CI
-$ heroku open
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:luisg210/test-api-luis.git main:gh-pages
+cd -
+
+# # version change
+# $ git init
+# $ git add .
+# $ git commit -m "My site ready for deployment."
+
+# # creates a new app with a specified name
+# $ heroku apps:create example
+
+# # set buildpack for static sites
+# $ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-static.git
+
+# # publish site
+# $ git push heroku main
+
+# # opens a browser to view the Dashboard version of Heroku CI
+# $ heroku open
